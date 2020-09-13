3c7de87ba2b540418bc1951f654be1aca1e4d982
JohnnyXcode <fernandesj92@yahoo.com>
2020-09-13 20:18:37 +0530

    Updates

---
+++ precache-manifest.333c5329693d453cf91e27fd9b89506d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e889738974e75748a7bce2f85e38ebc6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4cce57b154a4623d5a26",
+    "url": "/portfolio/static/css/main.8c73ac4b.chunk.css"
+  },
+  {
+    "revision": "2f396eb5592c9902d293",
+    "url": "/portfolio/static/js/2.3e2ae8c8.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio/static/js/2.3e2ae8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cce57b154a4623d5a26",
+    "url": "/portfolio/static/js/main.06ba16a9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
